05f292c420a8e599b87f07de80635651caa776e6
Eugen Zha <nightskylark@users.noreply.github.com>
2018-09-10 14:43:46 +0300

    Fix quotes once again

--- src/add-devextreme/index.ts
+++ src/add-devextreme/index.ts
@@ -50,8 +50,8 @@ function addDevExtremeCSS(host: Tree, options: any) {
     const projectBuildOptopns = config['projects'][projectName]['architect']['build']['options'];
     const projectSytles = projectBuildOptopns['styles'];
 
-    projectSytles.unshift("node_modules/devextreme/dist/css/dx.light.css");
-    projectSytles.unshift("node_modules/devextreme/dist/css/dx.common.css");
+    projectSytles.unshift('node_modules/devextreme/dist/css/dx.light.css');
+    projectSytles.unshift('node_modules/devextreme/dist/css/dx.common.css');
 
     projectBuildOptopns['styles'] = makeArrayUnique(projectSytles);
     return config;
