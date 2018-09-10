f1ae3ee54d42ca448bb6e622ada1a1dcfab7f0a1
Eugen Zha <nightskylark@users.noreply.github.com>
2018-09-10 14:41:40 +0300

    Fix quotes

--- src/utility/latest-versions.ts
+++ src/utility/latest-versions.ts
@@ -1,5 +1,5 @@
 // TODO: implement
 export const latestVersions = {
-  devextreme: '18.2-unstable',
-  "devextreme-angular": '18.2-unstable'
+  'devextreme': '18.2-unstable',
+  'devextreme-angular': '18.2-unstable'
 };
