69492a9f50734c47f82cc2a08720606b34c1c792
Vladimir <kuznetsov.vladimir.n@gmail.com>
2018-09-28 10:53:33 +0300

    Add angular crud component (#15)
    * add angular crud component
    
    * Improvements after review
    
    * Improvements after review
    
    * Improvements after review
    
    * Improvements after review
    
    * Improvements after review
    
    * corrected description

---
+++ src/add-angular-crud-component/component-options-model.ts
@@ -0,0 +1,12 @@
+export interface ComponentOptionsModel {
+  [key:string]: any;
+  controlToScaffold: string;
+  columns: string[];
+  editing: EditinigModel;
+}
+
+export interface EditinigModel {
+  allowAdding: boolean;
+  allowUpdating: boolean;
+  allowDeleting: boolean;
+}
---
+++ src/utility/ng-module-utils.ts
@@ -0,0 +1,43 @@
+import { Rule, Tree, SchematicsException } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import { ModuleOptions, findModuleFromOptions, buildRelativePath } from '@schematics/angular/utility/find-module';
+import { addImportToModule, addDeclarationToModule } from '@schematics/angular/utility/ast-utils';
+import { InsertChange } from '@schematics/angular/utility/change';
+import * as ts from 'typescript';
+
+export function addImportToParentModule(options: ModuleOptions, widget: string): Rule {
+  return (tree: Tree) => {
+    addImport(tree, options, widget);
+    return tree;
+  };
+}
+
+function addImport(tree: Tree, options: ModuleOptions, widget: string) {
+  const modulePath = findModuleFromOptions(tree, options) || '';
+  const moduleContent = tree.read(modulePath);
+  if (moduleContent === null) {
+    throw new SchematicsException(`File ${modulePath} does not exist.`);
+  }
+  const sourceText = moduleContent.toString('utf-8');
+  const source = ts.createSourceFile(modulePath, sourceText, ts.ScriptTarget.Latest, true);
+  const componentPath = `/${options.path}/`
+            + strings.dasherize(options.name) + '/'
+            + strings.dasherize(options.name) + '.component';
+  const relativePath = buildRelativePath(modulePath, componentPath);
+
+  const declarationRecorder = tree.beginUpdate(modulePath);
+
+  const classifiedComponent = 'Dx' + widget + 'Module';
+  for (const change of addImportToModule(source, modulePath, classifiedComponent, 'devextreme-angular')) {
+    if (change instanceof InsertChange)
+      declarationRecorder.insertLeft(change.pos, change.toAdd);
+  }
+
+  const classifiedName = strings.classify(options.name + 'Component');
+  for (const change of addDeclarationToModule(source, modulePath, classifiedName, relativePath)) {
+    if (change instanceof InsertChange)
+      declarationRecorder.insertLeft(change.pos, change.toAdd);
+  }
+
+  tree.commitUpdate(declarationRecorder);
+};
